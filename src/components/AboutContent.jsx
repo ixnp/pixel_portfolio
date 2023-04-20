d99a3774ebc6e8a3c9de2da7cec3a76e0d464de1
ixnp <enprocopios@gmail.com>
2023-04-20 15:59:44 -0700

    updated layout

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -4,19 +4,21 @@ import portraitMobile from "../assets/portrait_8bit.png"
 import imageMail from "../assets/bit-mailx2.png"
 import imageLinkedInx2 from "../assets/bit-linkedInx2.png"
 import imageGitx2 from "../assets/bit-gitx2.png"
+import imageResume2 from "../assets/bit-resumex2.png"
 
 
 function AboutContent() {
     return(
         <>
           <img id="portrait" src={portrait}></img>
-          <div>
+          <div class="about-content">
             <h2>Ix Procopios</h2>
             <h2>Software Engineer</h2>
             <ul>
               <li><img src={imageMail}/></li>
               <li><img src={imageLinkedInx2}/></li>
               <li><img src={imageGitx2}/></li>
+              <li><img src={imageResume2}/></li>
             </ul>
           </div>
         
